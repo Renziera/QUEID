0d209d2f8a586191a96702e8db484260be667e99
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-10-13 20:24:16 +0700

    Bottom Navbar

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:queid/page_home.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,13 +7,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _index = 0;
+
+  final List<Widget> _pages = [
+    HomePage(),
+    SizedBox.shrink(),
+    SizedBox.shrink(),
+    SizedBox.shrink(),
+    SizedBox.shrink(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('QUE.ID'),
       ),
-      
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (x) {
+          setState(() {
+            _index = x;
+          });
+        },
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.monetization_on),
+            title: Text('Sesuatu'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            title: Text('Entah'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.airplanemode_active),
+            title: Text('Pesawat'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text('Akun'),
+          ),
+        ],
+      ),
     );
   }
 }
